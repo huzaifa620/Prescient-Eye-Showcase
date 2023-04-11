341b814ea46d06eadd84c256120412b2561c2b67
Ghost620 <faghost6201@gmail.com>
2023-04-12 03:56:40 +0500

    Update Home.js

--- prescient-eye/src/Home.js
+++ prescient-eye/src/Home.js
@@ -9,21 +9,32 @@ const Home = () => {
     const videoRef = useRef(null);
     const [currentTime, setCurrentTime] = useState(0);
 
-    useEffect(() => {
-    const video = videoRef.current;
+    const [name, setName] = useState('-')
+    const [age, setAge] = useState('-')
+    const [nationality, setNationality] = useState('-')
+    const [history, setHistory] = useState('-')
+    const [org, setOrg] = useState('-')
 
-    const handleTimeUpdate = () => {
-        if (video.currentTime >= 5) {
-        setShowImage(true)
+    useEffect(() => {
+        const video = videoRef.current;
+
+        const handleTimeUpdate = () => {
+            if (video.currentTime >= 5) {
+                setShowImage(true)
+                setName('Hasna Aït Boulahcen')
+                setAge('26')
+                setNationality('French')
+                setHistory('Hasna Aït Boulahcen was a French-Moroccan woman who was born on January 1, 1989, in Clichy-la-Garenne, a suburb of Paris, France. She was known for her involvement in the November 2015 Paris attacks, which claimed the lives of 130 people.')
+                setOrg('ISIS (Daesh)')
+            };
+
         };
 
-    };
+        video.addEventListener('timeupdate', handleTimeUpdate);
 
-    video.addEventListener('timeupdate', handleTimeUpdate);
-
-    return () => {
-        video.removeEventListener('timeupdate', handleTimeUpdate);
-    };
+        return () => {
+            video.removeEventListener('timeupdate', handleTimeUpdate);
+        };
     }, []);
 
     return (
@@ -35,7 +46,7 @@ const Home = () => {
                     <source src={video} type="video/mp4" />
                     </video>
                 </div>
-                <div className='text-lg text-red-500 font-semibold'>
+                <div className='text-lg font-semibold'>
                     Live video feed
                 </div>
             </div>
@@ -50,22 +61,22 @@ const Home = () => {
                     className='h-80 w-80 object-cover'
                 />
 
-                <div className='dark:bg-[#f1f3f2] dark:text-[#141414] bg-gray-400 p-4 max-w-[60%] h-[65%] flex flex-col justify-center shadow-2xl'>
-                    <div className='flex flex-col border-b border-[#141414] py-4'>
-                        <h1 className='text-2xl font-bold tracking-widest'>Hasna Aït Boulahcen</h1>
-                        <p className='text-base'>Age: 26</p>
+                <div className='bg-[#141414] dark:bg-[#f1f3f2] text-[#f1f3f2] dark:text-[#141414] p-4 max-w-[60%] min-w-[60%] h-[65%] flex flex-col justify-center shadow-2xl'>
+                    <div className='flex flex-col border-b dark:border-[#141414] py-4'>
+                        <h1 className='text-2xl font-bold tracking-widest'> {name} </h1>
+                        <p className='text-base'>Age: {age}</p>
                     </div>
-                    <div className='flex flex-col border-b border-[#141414] py-4'>
+                    <div className='flex flex-col border-b dark:border-[#141414] py-4'>
                         <h1 className='text-lg font-bold tracking-widest'>NATIONALITY</h1>
-                        <p className='text-base'>French</p>
+                        <p className='text-base'> {nationality} </p>
                     </div>
-                    <div className='flex flex-col border-b border-[#141414] py-4'>
+                    <div className='flex flex-col border-b dark:border-[#141414] py-4'>
                         <h1 className='text-lg font-bold tracking-widest'>HISTORY</h1>
-                        <p className='text-justify text-lg'>Hasna Aït Boulahcen was a French-Moroccan woman who was born on January 1, 1989, in Clichy-la-Garenne, a suburb of Paris, France. She was known for her involvement in the November 2015 Paris attacks, which claimed the lives of 130 people.</p>
+                        <p className='text-justify text-lg'> {history} </p>
                     </div>
                     <div className='text-lg flex flex-col py-4'>
                         <h1 className='font-bold tracking-widest'>ORGANIZATION</h1>
-                        <p className='text-sm'>ISIS (Daesh)</p>
+                        <p className='text-sm'> {org} </p>
                     </div>
                 </div>
 
