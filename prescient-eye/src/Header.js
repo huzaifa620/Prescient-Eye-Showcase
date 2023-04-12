4faf26b51e2fb287befdad7054aa862949ad6222
Ghost620 <faghost6201@gmail.com>
2023-04-12 20:38:41 +0500

    icons fill and stroke colors dynamic

--- prescient-eye/src/Header.js
+++ prescient-eye/src/Header.js
@@ -13,6 +13,9 @@ const Header = () => {
     setisDark(!isDark)
   }
 
+  const fillColor = isDark ? 'white' : 'black'
+  const strokeColor = isDark ? 'black' : 'white'
+  
   const [openMenu, setOpenMenu] = useState(false);
 
   return (
@@ -48,7 +51,7 @@ const Header = () => {
 
           <a href="#contactUs" className='hidden md:flex space-x-1 mt-[10%] items-center bg-transparent px-3 py-1 rounded-3xl border border-[#141414] dark:border-[#f1f3f2]'>
 
-            <svg xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 24 24" strokeWidth={1.5} stroke="black" className="w-4 h-4">
+            <svg xmlns="http://www.w3.org/2000/svg" fill={fillColor} viewBox="0 0 24 24" strokeWidth={1.5} stroke={strokeColor} className="w-4 h-4">
               <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 9v.906a2.25 2.25 0 01-1.183 1.981l-6.478 3.488M2.25 9v.906a2.25 2.25 0 001.183 1.981l6.478 3.488m8.839 2.51l-4.66-2.51m0 0l-1.023-.55a2.25 2.25 0 00-2.134 0l-1.022.55m0 0l-4.661 2.51m16.5 1.615a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V8.844a2.25 2.25 0 011.183-1.98l7.5-4.04a2.25 2.25 0 012.134 0l7.5 4.04a2.25 2.25 0 011.183 1.98V19.5z" />
             </svg>
 
