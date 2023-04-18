bc5f861845a5c710fc047398eda910533f0ad5cf
Muhammad Huzaifa <79418234+huzaifa620@users.noreply.github.com>
2023-04-19 03:17:01 +0500

    Update App.js

--- prescient-eye/src/App.js
+++ prescient-eye/src/App.js
@@ -11,9 +11,8 @@ function App() {
 
       <section id='home' className=''>
         <div className="relative bg-cover bg-center">
-          <div className="absolute inset-0 mt-[11%] hidden md:flex justify-center z-0 text-[300px] font-semibold opacity-10 text-black dark:text-white">
+          <div className="absolute inset-0 mt-[5%] hidden md:flex justify-center z-0 text-[300px] font-semibold opacity-10 text-black dark:text-white">
             <img className='rounded-full' src={logo} alt='' />
-            {/* Prescient Eye */}
           </div>
           <div className="relative z-10">
             <Home />
