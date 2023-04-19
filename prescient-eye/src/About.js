b28fc01ac8084952361b1dc9aa1d417a3ee10a90
Muhammad Huzaifa <79418234+huzaifa620@users.noreply.github.com>
2023-04-19 06:20:26 +0500

    image slider

--- prescient-eye/src/About.js
+++ prescient-eye/src/About.js
@@ -3,14 +3,20 @@ import ImageSlider from './ImageSlider'
 import ListSlider from './ListSlider'
 
 const About = () => {
+  const images = [
+    "https://via.placeholder.com/300x200",
+    "https://via.placeholder.com/300x200/FF0000",
+    "https://via.placeholder.com/300x200/0000FF",
+  ];
+
   return (
     <div className='flex w-full space-x-6 p-8'>
         <div className='w-1/2'>
-            <ImageSlider />
+            <ImageSlider images={images} />
         </div>
 
         <div className='w-1/2'>
-            <ListSlider />
+            {/* <ListSlider /> */}
         </div>
     </div>
   )
--- prescient-eye/src/ImageSlider.js
+++ prescient-eye/src/ImageSlider.js
@@ -1,38 +1,68 @@
-import React, { useState } from 'react';
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+import { useState, useEffect } from "react";
+import { motion } from "framer-motion";
 
-const ImageSlider = () => {
-  const [currentSlide, setCurrentSlide] = useState(0);
+function ImageSlider() {
+  const [currentImage, setCurrentImage] = useState(0);
 
-  const settings = {
-    dots: true,
-    arrows: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    beforeChange: (current, next) => setCurrentSlide(next),
+  const prevImage = () => {
+    setCurrentImage(currentImage === 0 ? images.length - 1 : currentImage - 1);
   };
 
+  const nextImage = () => {
+    setCurrentImage(currentImage === images.length - 1 ? 0 : currentImage + 1);
+  };
+  
   const images = [
     "https://d33wubrfki0l68.cloudfront.net/dd23708ebc4053551bb33e18b7174e73b6e1710b/dea24/static/images/wallpapers/shared-colors@2x.png",
     "https://d33wubrfki0l68.cloudfront.net/49de349d12db851952c5556f3c637ca772745316/cfc56/static/images/wallpapers/bridge-02@2x.png",
     "https://d33wubrfki0l68.cloudfront.net/594de66469079c21fc54c14db0591305a1198dd6/3f4b1/static/images/wallpapers/bridge-01@2x.png"
   ];
+  useEffect(() => {
+    // Set initial opacity to 0
+    const image = document.querySelector(".slider-img");
+    image.style.opacity = 0;
+
+    // Animate opacity to 1
+    const animation = image.animate([{ opacity: 0 }, { opacity: 1 }], {
+      duration: 500,
+      fill: "forwards"
+    });
+
+    return () => {
+      // Stop animation when component unmounts
+      animation.cancel();
+    }
+  }, [currentImage]);
 
   return (
-    <div className="relative">
-      <Slider {...settings}>
-        {images?.map((image, index) => (
-          <div key={index}>
-            <img src={image} alt={`image-${index}`} className="h-auto w-full" />
-          </div>
-        ))}
-      </Slider>
-    </div>
+    <motion.div className="relative flex">
+      <motion.img
+        key={currentImage}
+        src={images[currentImage]}
+        alt="slider"
+        className="w-full h-96 object-cover rounded-3xl slider-img"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.5 }}
+      />
+      <div className="absolute h-full flex w-full items-center justify-between p-4">
+        <button
+          onClick={prevImage}
+          className="z-10"
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"><path d="M12 2a10 10 0 1 0 10 10A10.011 10.011 0 0 0 12 2zm0 18a8 8 0 1 1 8-8 8.009 8.009 0 0 1-8 8z"/><path d="M13.293 7.293 8.586 12l4.707 4.707 1.414-1.414L11.414 12l3.293-3.293-1.414-1.414z"/></svg>
+          
+        </button>
+        <button
+          onClick={nextImage}
+          className="z-10"
+        > 
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"><path d="M12 2a10 10 0 1 0 10 10A10.011 10.011 0 0 0 12 2zm0 18a8 8 0 1 1 8-8 8.009 8.009 0 0 1-8 8z"/><path d="M9.293 8.707 12.586 12l-3.293 3.293 1.414 1.414L15.414 12l-4.707-4.707-1.414 1.414z"/></svg>
+          
+        </button>
+      </div>
+    </motion.div>
   );
-};
+}
 
 export default ImageSlider;
