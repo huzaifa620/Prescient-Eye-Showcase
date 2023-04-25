df1ceb51ed6f27797732e28737bb3fc03e424a09
Muhammad Huzaifa <79418234+huzaifa620@users.noreply.github.com>
2023-04-26 00:33:01 +0500

    Update Features.js

--- prescient-eye/src/Features.js
+++ prescient-eye/src/Features.js
@@ -6,12 +6,17 @@ const Features = () => {
 
   return (
     <div className='flex w-full space-x-6 p-24'>
-        <div className='w-1/2'>
+        <div className='w-1/2 flex flex-col items-center space-y-4'>
+          <h1 className='text-5xl tracking-widest font-bold'>
+            Robbery Detection
+          </h1>
           <ImageSlider />
         </div>
 
-        <div className='w-1/2'>
-            <ListSlider />
+        <div className='w-1/2 flex items-center justify-center'>
+          <div className="border rounded-3xl w-full flex items-center justify-center text-center shadow-2xl h-1/2 text-2xl">
+              Real-time autonomus monitoring and survellience
+          </div>
         </div>
     </div>
   )
