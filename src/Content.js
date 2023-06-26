95eb5d8e95008374687c8ec6cbd82dcc7453a2fe
Muhammad Huzaifa <79418234+huzaifa620@users.noreply.github.com>
2023-06-26 19:09:54 +0500

    Update Content.js

--- src/Content.js
+++ src/Content.js
@@ -7,11 +7,11 @@ import pistol from './pistol.jpg'
 import robbery from './robbery.jpg'
 
 const content = [
-    {"title": "Unattended Luggage Detection", "image": bags, "desc": "Real-time autonomous monitoring and survellience."},
+    {"title": "Unattended Luggage Detection", "image": bags, "desc": "Real-time autonomous monitoring and surveillance."},
     {"title": "Suspected Car Detection", "image": cars, "desc": "Offline processing capabilities for data securities."},
     {"title": "Autonomous Number Plate Detection", "image": car, "desc": "Encrypted transmission of data from CCTV to Central Server and analysis system."},
     {"title": "Unattended Vehicle Identification", "image": openWorld, "desc": "Complete edge capabilities over data protection and threat analysis."},
-    {"title": "Robber and Arms Detection", "image": robbery, "desc": "Unsupervised learning automation model for continous training and processing."},
+    {"title": "Robber and Arms Detection", "image": robbery, "desc": "Unsupervised learning automation model for continuous training and processing."},
     {"title": "Suspected Weapon Identification", "image": pistol, "desc": "99% accuracy in terms of anomaly detection."},
     {"title": "3D Face Regeneration", "image": faces, "desc": "Vision based analytics."} 
 ]
